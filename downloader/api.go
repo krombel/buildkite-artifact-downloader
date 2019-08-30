a1db386ec57438e405f7170d6cba3ae1dba82678
Krombel <krombel@krombel.de>
2019-08-30 02:26:51 +0200

    use internal netClient

--- downloader/api.go
+++ downloader/api.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"regexp"
 	"strconv"
@@ -31,7 +32,7 @@ type BuildkiteBuildArtifactInfo struct {
 }
 
 func (bd *BuildkiteHandler) getLatestBuildID() (int, error) {
-	resp, err := netClient.Head(
+	resp, err := bd.netClient.Head(
 		"https://buildkite.com/" + bd.buildkiteOrg + "/" + bd.buildkitePipeline + "/builds/latest?branch=develop&state=passed",
 	)
 	if err != nil {
@@ -56,7 +57,7 @@ func (bd *BuildkiteHandler) getBuildInfo() (*BuildkiteBuildInfo, error) {
 		"buildID": bd.buildID,
 		"url":     url,
 	}).Debug("Start buildInfo download")
-	bodyBytes, err := getData(url)
+	bodyBytes, err := bd.getData(url)
 	if err != nil {
 		return nil, err
 	}
@@ -76,7 +77,7 @@ func (bd *BuildkiteHandler) getArtifactInfo(jobID string) ([]BuildkiteBuildArtif
 		"jobID":   jobID,
 		"url":     url,
 	}).Info("Start artifactInfo download")
-	bodyBytes, err := getData(url)
+	bodyBytes, err := bd.getData(url)
 	if err != nil {
 		return nil, err
 	}
@@ -88,6 +89,25 @@ func (bd *BuildkiteHandler) getArtifactInfo(jobID string) ([]BuildkiteBuildArtif
 	parsedResponse := []BuildkiteBuildArtifactInfo{}
 	json.Unmarshal(bodyBytes, &parsedResponse)
 	return parsedResponse, nil
+}
+
+func (bd *BuildkiteHandler) getData(url string) (bodyBytes []byte, err error) {
+	buildResponse, err := bd.netClient.Get(url)
+	if err != nil {
+		log.Fatal("GET failed", err)
+		return nil, err
+	}
+	defer buildResponse.Body.Close()
+
+	if buildResponse.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Could not get data")
+	}
+
+	bodyBytes, err = ioutil.ReadAll(buildResponse.Body)
+	if err != nil {
+		return nil, err
+	}
+	return bodyBytes, nil
 }
 
 func (bd *BuildkiteHandler) downloadArtifact(artifact BuildkiteBuildArtifactInfo, destPath string) error {
@@ -109,7 +129,7 @@ func (bd *BuildkiteHandler) downloadArtifact(artifact BuildkiteBuildArtifactInfo
 	}).Info("Start artifact download")
 
 	// Get the data
-	resp, err := netClient.Get("https://buildkite.com" + artifact.URL)
+	resp, err := bd.netClient.Get("https://buildkite.com" + artifact.URL)
 	if err != nil {
 		return fmt.Errorf("Cannot download to %s ('%s')", destPath, err)
 	}
